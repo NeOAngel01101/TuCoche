e65f9a81fa39baf88316b19816da22045fbcfefa
NeOAngel01101 <rayzzerv7@gmail.com>
2018-03-11 21:30:47 +0100

    cambio

--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -6,16 +6,5 @@
     </div>
     @include('coche.coche')
 
-
-        <form action="/{{ $user->username }}/dms" method="post">
-            {{ csrf_field() }}
-            <textarea name="message" id="message" rows="5"></textarea>
-            <button type="submit" class="button">Mensaje Directo</button>
-        </form>
-
-    @if( $conversation )
-        <a href="{{ route('conversation.show', $conversation->id) }}">Mensajes</a>
-    @endif
-
     <div class="text-center">{{ $coches->links('pagination::bootstrap-4') }}</div>
 @endsection
