c7ce8449a682ad94490f6d6efa4515bcc51c0eba
NeOAngel01101 <rayzzerv7@gmail.com>
2018-03-11 16:50:24 +0100

    agrupado las routas segun auth

--- routes/web.php
+++ routes/web.php
@@ -10,39 +10,36 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/profile/delete/{id}', 'ProfileController@borrarUsuario')->middleware('auth');
-Route::get('/coches/create', 'CochesController@create')->middleware('auth');
-Route::get('/administrar', 'CochesController@vercochescreados')->middleware('auth');
-Route::get('/administrar/delete/{id}', 'CochesController@deleteCoche')->middleware('auth');
+Route::group(['middleware' => 'auth'], function () {
 
-Route::get('/administrar/editarcoche/{id}', 'CochesController@edit')->middleware('auth');
-Route::put('/administrar/update/{id}', 'CochesController@update')->middleware('auth');
+    Route::get('/profile/delete/{id}', 'ProfileController@borrarUsuario');
+    Route::get('/coches/create', 'CochesController@create');
+    Route::get('/administrar', 'CochesController@vercochescreados');
+    Route::get('/administrar/delete/{id}', 'CochesController@deleteCoche');
+
+    Route::get('/administrar/editarcoche/{id}', 'CochesController@edit');
+    Route::put('/administrar/update/{id}', 'CochesController@update');
+    Route::post('/coches/create', 'CochesController@store');
+    Route::get('/profile/', 'ProfileController@index');
+
+    Route::get('/profile/edit', 'UsersController@profile')->name('profile');
+    Route::get('/profile/account', 'UsersController@edit')->name('profile.account');
+    Route::patch('/profile/account', 'UsersController@update');
+    Route::get('/profile/password', 'UsersController@edit')->name('profile.password');
+    Route::patch('/profile/password', 'UsersController@update');
+    Route::get('/profile/avatar', 'UsersController@edit')->name('profile.avatar');
+
+    Route::get('/conversations/{conversation}', 'UsersController@showConversation')->name('conversation.show');
+    Route::post('/{user}/dms', 'UsersController@sendPrivateMessage');
+});
 
 
 Route::get('/coches', 'CochesController@show');
 Route::get('/coches/{tipo}', 'CochesController@showtipos');
 
 Route::get('/coches/buscar/{id}', 'CochesController@detalle');
-Route::post('/coches/create', 'CochesController@store')->middleware('auth');
 
 Route::get('/user/{user}', 'UsersController@buscar');
-
-Route::get('/profile/', 'ProfileController@index')->name('auth');
-
-
-Route::get('/profile/edit', 'UsersController@profile')->name('profile');
-Route::get('/profile/account', 'UsersController@edit')->name('profile.account');
-Route::patch('/profile/account', 'UsersController@update');
-Route::get('/profile/password', 'UsersController@edit')->name('profile.password');
-Route::patch('/profile/password', 'UsersController@update');
-Route::get('/profile/avatar', 'UsersController@edit')->name('profile.avatar');
-
-Route::get('/conversations/{conversation}', 'UsersController@showConversation')->name('conversation.show');
-Route::post('/{user}/dms', 'UsersController@sendPrivateMessage');
-
-
-
-
 
 Auth::routes();
 
